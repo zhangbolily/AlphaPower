0d48aa76e220254bfb2abcf7d362e8beccd868a8
ballchang <zhangbolily@gmail.com>
2025-03-28 23:32:09 +0800

    新增Mock工作者类和抽象工作者类，定义工作者行为接口，支持任务调度和资源管理
    Change-Id: Iff45cdd35bfdefa3d60cc6b041799f557c5bde2d

---
+++ src/alphapower/engine/simulation/task/worker.py
@@ -0,0 +1,77 @@
+"""
+@file: worker.py
+@brief: Mock工作者类
+@details:
+    该模块定义了一个Mock工作者类，用于模拟工作者的行为。
+@note:
+    该模块是 AlphaPower 引擎的一部分
+"""
+
+import asyncio
+from typing import List, Optional
+
+from alphapower.internal.entity import SimulationTask, SimulationTaskStatus
+
+from .scheduler_abc import AbstractScheduler
+from .worker_abc import AbstractWorker
+
+
+class MockWorker(AbstractWorker):
+    """
+    Mock工作者类，用于模拟工作者的行为。
+    """
+
+    def __init__(self, work_time: int = 1, job_slots: int = 1):
+        """
+        初始化Mock工作者。
+        """
+        self.scheduler: Optional[AbstractScheduler] = None
+        self.tasks: List[SimulationTask] = []
+        self.running: bool = False
+        self.running_lock: asyncio.Lock = asyncio.Lock()
+        self.shutdown: bool = False
+        self.work_time: int = work_time
+        self.job_slots: int = job_slots
+
+    def set_scheduler(self, scheduler: AbstractScheduler) -> None:
+        """
+        设置调度器。
+        :param scheduler: 调度器实例
+        """
+        self.scheduler = scheduler
+
+    async def run(self):
+        """
+        运行工作者，执行任务。
+        """
+        self.running = True
+
+        async with self.running_lock:
+            while not self.shutdown:
+                if self.tasks:
+                    raise RuntimeError(
+                        "任务列表不为空，存在已完成任务未清理，无法运行工作者。"
+                    )
+                if self.scheduler is None or not isinstance(
+                    self.scheduler, AbstractScheduler
+                ):
+                    raise RuntimeError("调度器未设置或类型不正确。")
+
+                self.tasks = await self.scheduler.schedule(batch_size=self.job_slots)
+                await asyncio.sleep(self.work_time)  # 模拟异步操作
+                # 模拟任务完成
+                for task in self.tasks:
+                    task.status = SimulationTaskStatus.COMPLETE
+
+    async def stop(self, cancel_tasks=False):
+        """
+        停止工作者，清理资源。
+        :param cancel_tasks: 是否取消任务
+        """
+        self.shutdown = True
+
+        async with self.running_lock:
+            await asyncio.sleep(self.work_time)
+            if cancel_tasks:
+                self.tasks.clear()
+            self.running = False
---
+++ src/alphapower/engine/simulation/task/worker_abc.py
@@ -0,0 +1,48 @@
+"""
+@file
+@brief 抽象工作者类
+@details
+    该模块定义了一个抽象工作者类，所有具体的工作者都应该继承这个类。
+@note
+    该模块是 AlphaPower 引擎的一部分
+"""
+
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from .scheduler_abc import AbstractScheduler
+
+
+class AbstractWorker(ABC):
+    """
+    抽象类，用于定义工作者的通用接口。
+    """
+
+    @abstractmethod
+    def set_scheduler(self, scheduler: AbstractScheduler) -> None:
+        """
+        设置调度器。
+
+        :param scheduler: 调度器实例
+        """
+
+    @abstractmethod
+    async def run(self) -> None:
+        """
+        运行工作者，执行任务。
+        """
+
+    @abstractmethod
+    async def stop(self, cancel_tasks: bool = False) -> None:
+        """
+        停止工作者，清理资源。
+        """
+
+    @abstractmethod
+    def add_task_complete_callback(self, callback: Callable) -> None:
+        """
+        添加任务完成回调函数。
+        TODO: 回调函数的入参类型需要确定一下
+
+        :param callback: 回调函数
+        """
