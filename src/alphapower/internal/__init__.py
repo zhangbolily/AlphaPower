bf4eaa8e0b52bd0e1886b6a257c826ea5a9be4e2
ballchang <zhangbolily@gmail.com>
2025-04-24 20:03:26 +0800

    移除不必要的 Propagation 和 Transactional 导入
    精简模块导入，提升代码可读性，减少不必要的依赖。
    
    Change-Id: I4c3a6a05419a4fdf715b56a6c7a3f8e1109a1787

--- src/alphapower/internal/__init__.py
+++ src/alphapower/internal/__init__.py
@@ -5,11 +5,8 @@ This module is part of the AlphaPower package.
 __all__ = [
     "exception_handler",
     "log_time_elapsed",
-    "Propagation",
     "get_logger",
-    "Transactional",
 ]
 from .logging import get_logger
 from .wraps.exception import exception_handler
 from .wraps.log_time_elapsed import log_time_elapsed
-from .wraps.transactional import Propagation, Transactional
